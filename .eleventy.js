143b604b5d172ce6b4b0fe508cb49b0f6046a17e
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2020-08-16 22:34:13 +0200

    add

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,14 @@ const htmlmin = require('html-minifier');
 const dateFns = require('date-fns');
 const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+const pluginTOC = require('eleventy-plugin-toc');
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.setLibrary('md', markdownIt().use(markdownItAnchor));
+  eleventyConfig.addPlugin(pluginTOC);
+
   eleventyConfig.addPlugin(syntaxHighlight);
 
   eleventyConfig.addPlugin(lazyImagesPlugin, {
